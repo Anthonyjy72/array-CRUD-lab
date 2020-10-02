149cb75cecc89b85c48f979ec55a4d1899e7dcb7
Anthonyjy72 <anthony_jy72@yahoo.com>
2020-10-02 11:22:50 -0500

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
+    new_array = []
+  end
   
-end
-
-def create_an_array
+  def create_an_array
+    array = ["red", "gold", "black", "green"]
+  end
   
-end
-
-def add_element_to_end_of_array(array, element)
+  def add_element_to_end_of_array(array, element)
+    array = ["red", "gold", "black"]
+    array.push("arrays!") 
+  end
   
-end
-
-def add_element_to_start_of_array(array, element)
+  def add_element_to_start_of_array(array, element)
+    array = ["red", "gold", "black"]
+    array.unshift("wow")
+  end
   
-end
-
-def remove_element_from_end_of_array(array)
+  def remove_element_from_end_of_array(array)
+    array = ["red", "gold", "black", "arrays!"]
+    array.pop
+  end
   
-end
-
-def remove_element_from_start_of_array(array)
+  def remove_element_from_start_of_array(array)
+    array = ["wow", "red", "gold", "black"]
+    array.shift
+  end
   
-end
-
-def retrieve_element_from_index(array, index_number)
+  def retrieve_element_from_index(array, index_number)
+    words = ["not", "here", "am", "gone"]
+    words[2]
+  end
   
-end
-
-def retrieve_first_element_from_array(array)
+  def retrieve_first_element_from_array(array)
+    words = ["wow", "cool", "awesome"]
+    words[0]
+  end
   
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
+  def retrieve_last_element_from_array(array)
+    words = ["wow", "cool", "arrays!"]
+    words[2]
+  end
